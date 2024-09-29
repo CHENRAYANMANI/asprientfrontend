7b8b24e1e29506e38e44b806eac1a1e1b4f8a633
chenrayan <chenrayanmani@gmail.com>
2024-09-29 20:27:33 +0530

    fg

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { store } from "./app/store";
+
 import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
